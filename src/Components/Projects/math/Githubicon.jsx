d988283a7731cfbdc62ed0aacfd7c27a6da9cce8
Wahab901278 <abdulwahab901278@gmail.com>
2024-07-28 21:52:09 +0500

    Made changes

--- src/Components/Projects/math/Githubicon.jsx
+++ src/Components/Projects/math/Githubicon.jsx
@@ -2,8 +2,6 @@
 import React from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faGithub } from "@fortawesome/free-brands-svg-icons";
-import "/home/wahab901278/Important-stuff/MySite-Frontend/personal-portfolio/src/index.css";
-
 const Githubicon = () => {
   return (
     <div className="social-media-icons">
