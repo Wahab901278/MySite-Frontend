ed5ab2fe347ab3cc6aa210cdfd0ca7bc027319cb
Wahab901278 <abdulwahab901278@gmail.com>
2024-07-28 20:35:38 +0500

    changed paths of resume.pdf

--- src/Components/Resume/Resume.jsx
+++ src/Components/Resume/Resume.jsx
@@ -2,7 +2,6 @@ import React from "react";
 import Topnavbar from "../Home/Topnavbar";
 import "bootstrap/dist/css/bootstrap.min.css";
 import Lfooterc from "../Contact/Lfooterc";
-import resumePDF from "/home/wahab901278/resume.pdf";
 import resumeImage01 from "./images/resume.jpg";
 
 import "./Resume.css";
@@ -14,7 +13,7 @@ const Resume = () => {
       <div className="resume-l flex">
         {/* <h2 id="Experience">Experience</h2> */}
         <a
-          href={resumePDF}
+          href="/resume.pdf"
           download="Abdul Wahab.pdf"
           id="download-button"
           className="download-button"
